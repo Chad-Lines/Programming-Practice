cb05c2ba3e73cc4508b1977657d2a6fe94be025a
Chad <chad.lines1@gmail.com>
2022-07-29 16:44:39 -0600

    0.001 - Choice Handling and Binary Search

---
+++ Practice/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal static class BinarySearch
+    {
+        // Perform a binary search on an array of numbers, looking for the 
+        // target number
+        public static int Search(int[] nums, int target)
+        {
+            // setting the search space from 0 to the length of the array
+            int left = 0;
+            int right = nums.Length - 1;
+
+            // Loop 
+            while (left <= right)
+            {
+                // Determine the mid-value in the search to compare with the target
+                int mid = (left + right) / 2;
+
+                // If the target is found, return it
+                if (target == nums[mid]) return mid;
+
+                // If the target is less than the mid, then set the right to mid - 1 (descreasing the search area)
+                else if (target < nums[mid]) right = mid - 1;
+
+                // If the target is more than the mid, then set the left to mid + 1 (descreasing the search area)
+                else left = mid + 1;                            
+            }
+
+            // The target doesn't exist in the array 
+            return -1;
+        }
+
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            int choice;
+
+            Console.WriteLine("\nWhat do you want to do? Enter the corresponding number:\n");
+            Console.WriteLine("1. Perform a Binary Search\n");
+            
+            string input = Console.ReadLine();            
+
+            if(Int32.TryParse(input, out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        BinarySearchSelection();
+                        break;
+                    default:
+                        Invalid();
+                        break;
+                }
+            }
+            else
+            {
+                Invalid();                
+            }
+        }
+
+        static void Invalid()
+        {
+            Console.WriteLine("\nPlease enter a number representing your selection.\n");
+            Main();
+        }
+
+        static void Again()
+        {
+            Console.WriteLine("\nDo you want to go again? [Y/N]\n");
+            string input = Console.ReadLine();
+
+            switch (input.ToUpper())
+            {
+                case "Y":
+                    Main();
+                    break;
+                case "N":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Invalid();
+                    break;
+            }
+        }
+
+        static void BinarySearchSelection()
+        {
+            int choice;
+            int[] nums = Enumerable.Range(1, 100).ToArray();
+            
+            Console.WriteLine("\nPlease enter a number between 1 and 100.\n");
+
+            string input = Console.ReadLine();
+
+            if (Int32.TryParse(input, out choice))
+            {
+                int idx = BinarySearch.Search(nums, choice);
+                Console.WriteLine("\n" + choice.ToString() + " found at index: " + idx.ToString() + "\n");
+                Again();
+            }
+            else
+            {
+                Invalid();
+            }
+
+        }
+    }
+}
