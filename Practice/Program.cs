cd23581f39ebc9883c090c647b7267dd55e5b35c
Chad <chad.lines1@gmail.com>
2022-07-29 17:12:16 -0600

    0.002 - Quick Sort

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -11,8 +11,11 @@ namespace Practice
             int choice;
 
             Console.WriteLine("\nWhat do you want to do? Enter the corresponding number:\n");
-            Console.WriteLine("1. Perform a Binary Search\n");
-            
+            Console.WriteLine("1. Perform a Binary Search");
+            Console.WriteLine("2. Quick Sort");
+
+            Console.WriteLine("\n");
+
             string input = Console.ReadLine();            
 
             if(Int32.TryParse(input, out choice))
@@ -21,6 +24,9 @@ namespace Practice
                 {
                     case 1:
                         BinarySearchSelection();
+                        break;
+                    case 2:
+                        QuickSortSelection();
                         break;
                     default:
                         Invalid();
@@ -32,7 +38,42 @@ namespace Practice
                 Invalid();                
             }
         }
+                
 
+        static void BinarySearchSelection()
+        {
+            int choice;
+            int[] nums = Enumerable.Range(1, 100).ToArray();
+            
+            Console.WriteLine("\nPlease enter a number between 1 and 100.\n");
+
+            string input = Console.ReadLine();
+
+            if (Int32.TryParse(input, out choice))
+            {
+                int idx = BinarySearch.Search(nums, choice);
+                Console.WriteLine("\n" + choice.ToString() + " found at index: " + idx.ToString() + "\n");
+                Again();
+            }
+            else Invalid();
+        }
+
+        static void QuickSortSelection()
+        {
+            int[] array = { 9, -3, 5, 2, 6, 8, -6, 1, 3 };
+            Console.Write("\nWe will quicksort the following numbers: ");
+            foreach (int i in array) { Console.Write("{0}", i); }
+
+            QuickSort.Sort(array, 0, array.Length - 1);
+
+            Console.Write("\nViola! ");
+            foreach (int i in array) { Console.Write("{0}", i); }
+            Console.Write("\n");
+
+            Again();
+        }
+
+        #region HouseKeeping
         static void Invalid()
         {
             Console.WriteLine("\nPlease enter a number representing your selection.\n");
@@ -57,27 +98,6 @@ namespace Practice
                     break;
             }
         }
-
-        static void BinarySearchSelection()
-        {
-            int choice;
-            int[] nums = Enumerable.Range(1, 100).ToArray();
-            
-            Console.WriteLine("\nPlease enter a number between 1 and 100.\n");
-
-            string input = Console.ReadLine();
-
-            if (Int32.TryParse(input, out choice))
-            {
-                int idx = BinarySearch.Search(nums, choice);
-                Console.WriteLine("\n" + choice.ToString() + " found at index: " + idx.ToString() + "\n");
-                Again();
-            }
-            else
-            {
-                Invalid();
-            }
-
-        }
+        #endregion
     }
 }
---
+++ Practice/QuickSort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice;
+
+internal class QuickSort
+{
+    public static void Swap(int[] arr, int i, int j)
+    {
+        // Quickly swap two indexes in an array
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int Partition(int[] a, int start, int end)
+    {
+        // Pick the right-most element as the pivot from the given array
+        int pivot = a[end];
+
+        // Elements less than the pivot are sent to the left
+        // Elements more than the pivot are sent to the right
+        // Equal elements can go either way
+        int pIndex = start;
+
+        for (int i = start; i < end; i++)
+        {
+            // each time we find an element less than or equal to the pivot,
+            // 'pIndex' is incremented, and that element would be placed
+            // before the pivot.
+            if (a[i] <= pivot)
+            {
+                Swap(a, i, pIndex);
+                pIndex++;
+            }
+        }
+
+        // swap 'pIndex' with pivot
+        Swap(a, end, pIndex);
+
+        return pIndex;
+    }
+
+    public static void Sort(int[] a, int start, int end)
+    {
+        // Make sure it's valid
+        if (start >= end) return;
+
+        // Rearrange the elements across pivot
+        int pivot = Partition(a, start, end);
+
+        // Recur on the subarray containing elements less than the pivot
+        Sort(a, start, pivot - 1);
+
+        // Recur on the subarray containing elements more than the pivot
+        Sort(a, pivot + 1, end);
+    }
+}
